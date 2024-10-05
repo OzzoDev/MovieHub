f60c57edd323198ef325f6f4f9050d08ed07f81d
oscar <oscar.burman005@gmail.com>
2024-10-05 21:07:19 +0200

    Nav and top image animation done

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ icon.addEventListener("click", (event) => {
   icon.classList.toggle("open");
   nav_ul.classList.toggle("show-ul");
 });
+
+const container = document.querySelector(".particle-container");
+
+for (let i = 0; i < 200; i++) {
+  const particle = document.createElement("div");
+  particle.classList.add("particle");
+
+  const topPosition = Math.random() * 100;
+  const leftPosition = Math.random() * 100;
+  particle.style.top = `${topPosition}%`;
+  particle.style.left = `${leftPosition}%`;
+
+  const animationDuration = Math.random() * 10 + 2;
+  particle.style.animationDuration = `${animationDuration}s`;
+
+  container.appendChild(particle);
+}
